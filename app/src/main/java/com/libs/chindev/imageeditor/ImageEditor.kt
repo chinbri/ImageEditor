a61ee1b672d70c10778a361e1118cd918b351a72
David Sanchez <dsbrio@indra.es>
2019-03-06 14:26:54 +0100

    resize bitmap

--- app/src/main/java/com/libs/chindev/imageeditor/ImageEditor.kt
+++ app/src/main/java/com/libs/chindev/imageeditor/ImageEditor.kt
@@ -8,13 +8,23 @@ import android.view.View
 import androidx.constraintlayout.widget.ConstraintLayout
 import kotlinx.android.synthetic.main.image_editor.view.*
 import android.graphics.drawable.BitmapDrawable
+import android.view.ViewTreeObserver
 import com.libs.chindev.imageeditor.paint.PaintBuilder
+import android.util.TypedValue
+import android.opengl.ETC1.getHeight
+import android.opengl.ETC1.getWidth
+import android.graphics.Bitmap
+import android.graphics.drawable.Drawable
+
+
+
+
 
 
 class ImageEditor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : ConstraintLayout(context, attrs, defStyleAttr) {
 
     companion object {
-        const val EVENT_DISTANCE = 10
+        const val EVENT_DISTANCE = 3
     }
 
     private val colorOne = context.resources.getColor(R.color.defaultColorOne)
@@ -31,6 +41,8 @@ class ImageEditor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : C
         init()
     }
 
+    var imageInitialized = false
+
     private fun init() {
         View.inflate(context, R.layout.image_editor, this)
 
@@ -38,69 +50,66 @@ class ImageEditor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : C
         options.inMutable = true
 
         //Create a new image bitmap and attach a brand new canvas to it
-        val tempBitmap = BitmapFactory.decodeStream(context.assets.open("map.png"), null, options)
-        var tempCanvas = Canvas(tempBitmap)
+        var tempBitmap = drawableToBitmap(context.resources.getDrawable(R.drawable.ic_launcher_background))
+//
+//        tempBitmap = Bitmap.createScaledBitmap(tempBitmap, ivMainImage.width, ivMainImage.height, false)
+////            BitmapFactory.decodeStream(context.assets.open("map.png"), null, options)
+//        val tempCanvas = Canvas(tempBitmap)
 
         val paint = PaintBuilder()
-            .setColor(colorOne)
+            .setColor(colorThree)
             .setStrokeWidth(20f)
             .build()
 
-        ivMainImage.setOnTouchListener(object : View.OnTouchListener {
+        ivMainImage.viewTreeObserver
+            .addOnGlobalLayoutListener(object : ViewTreeObserver.OnGlobalLayoutListener {
+                override fun onGlobalLayout() {
+                    ivMainImage.viewTreeObserver.removeOnGlobalLayoutListener(this)
 
-            override fun onTouch(v: View?, event: MotionEvent?): Boolean {
-                performClick()
+                    if(!imageInitialized){
+                        val aspectRatioBitmap = tempBitmap.width / tempBitmap.height
+                        val aspectRatioImageView = ivMainImage.width / ivMainImage.height
 
-                when (event?.action){
-
-                    MotionEvent.ACTION_DOWN -> {
-                        println("DOWN ${event.x}, ${event.y}")
-                    }
-
-                    MotionEvent.ACTION_MOVE -> {
-
-                        //Draw the image bitmap into the cavas
-                        tempCanvas.drawBitmap(tempBitmap, 0f, 0f, null)
-
-                        if(checkMovement(previousCoordinates, event)){
-
-                            tempCanvas.drawLine(
-                                previousCoordinates?.x ?: event.x,
-                                previousCoordinates?.y ?: event.y,
-                                event.x,
-                                event.y,
-                                paint)
-
+                        var newBitmapWidth = 0
+                        var newBitmapHeight = 0
+                        if(aspectRatioBitmap > aspectRatioImageView){
+                            newBitmapWidth = ivMainImage.width
+                            newBitmapHeight = ivMainImage.width / aspectRatioBitmap
+                        }else{
+                            newBitmapHeight = ivMainImage.height
+                            newBitmapWidth = ivMainImage.height * aspectRatioBitmap
                         }
 
-                        tempCanvas.save();
-                        tempCanvas.translate(0f, 0f);
-                        tempCanvas.restore();
+                        tempBitmap = Bitmap.createScaledBitmap(tempBitmap, newBitmapWidth, newBitmapHeight, false)
 
+                        imageInitialized = true
                     }
 
-                    MotionEvent.ACTION_UP -> {
 
-                        //Attach the canvas to the ImageView
-                        ivMainImage.setImageDrawable(BitmapDrawable(resources, tempBitmap))
+                    val tempCanvas = Canvas(tempBitmap)
 
+                    val bitmapWidth = tempBitmap.width
+                    val bitmapHeight = tempBitmap.height
+
+                    val currentImageViewRatio =
+                        ivMainImage.width.toFloat() / ivMainImage.height
+                    val currentImageRatio = bitmapWidth.toFloat() / bitmapHeight
+
+                    val layoutParams = ivMainImage.layoutParams
+                    if (currentImageRatio > currentImageViewRatio) {
+                        layoutParams.height = (ivMainImage.width / currentImageRatio).toInt()
+                    } else {
+                        layoutParams.width = (ivMainImage.height * currentImageRatio).toInt()
                     }
 
-                    else -> println("OTHER")
-                }
+                    ivMainImage.layoutParams = layoutParams
+                    ivMainImage.setImageBitmap(tempBitmap)
 
-                println("PREVIOUSEVENT ${previousCoordinates?.x} ${previousCoordinates?.y}  ---   EVENT ${event?.x} ${event?.y}")
-
-                if(previousCoordinates == null || checkMovement(previousCoordinates, event)){
-
-                    previousCoordinates = PointF(event!!.x, event.y)
+                    initializeMotionCapture(tempCanvas, tempBitmap, paint)
 
                 }
-
-                return true
-            }
-
-        })
+            })
+//        initializeMotionCapture(tempCanvas, tempBitmap, paint)
 
         ivColorOne.setOnClickListener {
 
@@ -114,6 +123,90 @@ class ImageEditor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : C
 
         }
 
+    }
+
+    fun drawableToBitmap(drawable: Drawable): Bitmap {
+
+        if (drawable is BitmapDrawable) {
+            return drawable.bitmap
+        }
+
+        val bitmap = Bitmap.createBitmap(drawable.intrinsicWidth, drawable.intrinsicHeight, Bitmap.Config.ARGB_8888)
+        val canvas = Canvas(bitmap)
+        drawable.setBounds(0, 0, canvas.width, canvas.height)
+        drawable.draw(canvas)
+
+        return bitmap
+    }
+
+    private fun convertToPixels(dip: Float): Float {
+        return TypedValue.applyDimension(
+            TypedValue.COMPLEX_UNIT_DIP,
+            dip,
+            resources.displayMetrics
+        )
+    }
+
+    private fun initializeMotionCapture(
+        tempCanvas: Canvas,
+        tempBitmap: Bitmap?,
+        paint: Paint
+    ) {
+        ivMainImage.setOnTouchListener(object : OnTouchListener {
+
+            override fun onTouch(v: View?, event: MotionEvent?): Boolean {
+                performClick()
+
+                when (event?.action) {
+
+                    MotionEvent.ACTION_DOWN -> {
+                        println("DOWN ${event.x}, ${event.y}")
+                    }
+
+                    MotionEvent.ACTION_MOVE -> {
+
+                        //Draw the image bitmap into the cavas
+                        tempCanvas.drawBitmap(tempBitmap, 0f, 0f, null)
+
+                        if (checkMovement(previousCoordinates, event)) {
+
+                            tempCanvas.drawLine(
+                                previousCoordinates?.x ?: event.x,
+                                previousCoordinates?.y ?: event.y,
+                                event.x,
+                                event.y,
+                                paint
+                            )
+
+                        }
+
+                        tempCanvas.save();
+                        tempCanvas.translate(0f, 0f);
+                        tempCanvas.restore();
+
+                        //Attach the canvas to the ImageView
+                        ivMainImage.setImageDrawable(BitmapDrawable(resources, tempBitmap))
+                    }
+
+                    MotionEvent.ACTION_UP -> {
+
+                    }
+
+                    else -> println("OTHER")
+                }
+
+                println("PREVIOUSEVENT ${previousCoordinates?.x} ${previousCoordinates?.y}  ---   EVENT ${event?.x} ${event?.y}")
+
+                if (previousCoordinates == null || checkMovement(previousCoordinates, event)) {
+
+                    previousCoordinates = PointF(event!!.x, event.y)
+
+                }
+
+                return true
+            }
+
+        })
     }
 
     fun checkMovement(previousEvent: PointF?, event: MotionEvent?): Boolean{
